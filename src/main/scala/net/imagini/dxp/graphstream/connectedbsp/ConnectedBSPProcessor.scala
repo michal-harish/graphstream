63d8a4e5086350230821e5ec8f90d442fab13914
Michal Harish <michal.haris@imagini.net>
2015-10-03 19:41:24 +0100

    NOJIRA Increasing BSP max iterations to 4

--- src/main/scala/net/imagini/dxp/graphstream/connectedbsp/ConnectedBSPProcessor.scala
+++ src/main/scala/net/imagini/dxp/graphstream/connectedbsp/ConnectedBSPProcessor.scala
@@ -17,7 +17,7 @@ import org.apache.donut.memstore.{MemStore, MemStoreMemDb}
 class ConnectedBSPProcessor(maxStateSizeMb: Int, minEdgeProbability: Double) {
 
   type MESSAGE = KeyedMessage[ByteBuffer, ByteBuffer]
-  val MAX_ITERATIONS = 3
+  val MAX_ITERATIONS = 4
   private val MAX_EDGES = 99
 
   val state: MemStore = new MemStoreMemDb(maxStateSizeMb)
